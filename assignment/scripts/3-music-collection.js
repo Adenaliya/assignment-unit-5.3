1f27c7a9f7ba3ea4f761862e5f587dd8599c9b9d
Aliya Aden <adenaliya17@gmail.com>
2024-10-27 15:58:45 -0500

    didn't fork correctly, but completed

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,46 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+let myCollection = [];
+console.log('whats in my collection',myCollection);
+function addToCollection(collection, title, artist, yearPublished) {
+    let album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    };
+    collection.push(album);
+    return album;
+};
+
+addToCollection(myCollection, 'lett the world know', 'mgm lett', 2022);
+addToCollection(myCollection, 'realer 2', 'youngboy Neverbroke again', 2022);
+addToCollection(myCollection, 'letter 2 my brother', 'lil kee', 2022);
+addToCollection(myCollection, 'el toro 2', 'est gee', 2023);
+addToCollection(myCollection, 'humble as ever', 'hunxho', 2023);
+addToCollection(myCollection, 'certified lover boy', 'drake', 2022);
+
+console.log('what in my collection now' , myCollection);
+
+function showCollection(collection) {
+  for (let album of collection) {
+      console.log(album.title + " by " + album.artist + ", published in " + album.yearPublished);
+  }
+}
+showCollection(myCollection);
+
+function findByArtist(collection, artist) {
+  let artistAlbums = []; 
+  for (let i = 0; i < collection.length; i++) {
+      if (collection[i].artist === artist) {
+          artistAlbums.push(collection[i]);
+      }
+  }
+  return artistAlbums; 
+};
+console.log(findByArtist(myCollection, 'mgm lett',));
+console.log(findByArtist(myCollection, 'lil baby'));
+console.log('mgm let is in my array and lil baby is not');
 
 
 
